fe0654c00306a7e9c0f30af3adc4e4a0e95aa402
IsanaKoto <isn.kumano@gmail.com>
2024-04-25 21:00:38 +0900

    選択UI の途中まで

--- gb/serial/serial.c
+++ gb/serial/serial.c
@@ -3,57 +3,45 @@
 #include <stdio.h>
 
 unsigned char buffer[32];
+const char *menuItems[] = { "AAAAAAAAAAAA", "BBBBBBB", "CCCCCCC", "DDDDDDDDDDD"};
+const uint8_t menuSize = sizeof(menuItems) / sizeof(menuItems[0]);
+uint8_t currentSelection = 0;
+uint8_t previousSelection = 0xFF; // 初期値は無効な選択肢に設定
 
-const unsigned char * const fromESP = (unsigned char *)0x7ffe;
-unsigned char * const toESP = (unsigned char *)0x7fff;
-
-void readFromESP(unsigned char * result) {
-    uint8_t i = 0;
-    result[0] = *fromESP; //First read only triggers the read mode on the ESP. The result is garbage and will be overwriten in the first run of the loop.
-    do {
-        int burn_a_few_cycles = i*i*i;
-        result[i] = *fromESP;
-    } while (result[i++] != '\0');
+void drawMenu() {
+    printf("IP:192.168.1.2", "SSID");
+    for (uint8_t i = 0; i < menuSize; i++) {
+        printf("  %s\n", menuItems[i]); // 全ての項目を通常状態で描画
+    }
 }
 
-void sendToESP(unsigned char * s) {
-    unsigned char * i = s;
-    do {
-        uint8_t burn_a_few_cycles;
-        //We need to send each character twice as the first write triggers the interrupt on the ESP, which will then wait and catch the second write
-        *toESP = *i;
-        for (burn_a_few_cycles = 0; burn_a_few_cycles < 1; burn_a_few_cycles++) {
-            //NOOP - otherwise we will be too fast for the ESP to catch the second write
-        }
-        *toESP = *i;
-        for (burn_a_few_cycles = 0; burn_a_few_cycles < 3; burn_a_few_cycles++) {
-            //NOOP - a little longer as the ESP needs to store the previous value
-        }
-    } while (*(i++) != '\0');
+void redrawSelection() {
+    if (previousSelection != 0xFF) {
+        printf("\x1B[%d;1H  %s\n", previousSelection + 1, menuItems[previousSelection]); // 前回の選択を通常状態で再描画
+    }
+    printf("\x1B[%d;1H> %s\n", currentSelection + 1, menuItems[currentSelection]); // 現在の選択をハイライトして再描画
+}
+
+void updateSelection(uint8_t joypadState) {
+    previousSelection = currentSelection;
+    if (joypadState & J_UP) {
+        if (currentSelection > 0) currentSelection--;
+    } else if (joypadState & J_DOWN) {
+        if (currentSelection < menuSize - 1) currentSelection++;
+    }
 }
 
 void main(void) {
-    puts("");
-    puts("there.oughta.be");
-    puts("  /a/wifi-game-boy");
-    puts("          -cartridge");
-    puts("");
-    puts("Serial test");
-    puts("");
+    DISPLAY_ON; // ゲームボーイのディスプレイをオンにする
+    drawMenu(); // 初期メニュー描画
+    redrawSelection(); // 初期選択をハイライト
 
     while(1) {
-        unsigned char buffer[256];
-        readFromESP(buffer);
-        if (buffer[0] != '\0') {
-            puts("   --Receiving--");
-            puts(buffer);
+        uint8_t joypadState = joypad();
+        if (joypadState & (J_UP | J_DOWN)) {
+            updateSelection(joypadState);
+            redrawSelection(); // 選択が変更された場合のみ矢印を移動
         }
-
-        if (joypad() & J_A) {
-            puts("    --Sending--");
-            gets(buffer);
-            sendToESP(buffer);
-        }
-        delay(100);
+        delay(100); // デバウンスのための小さな遅延
     }
 }
